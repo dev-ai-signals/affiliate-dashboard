9f621a226fa45647c051cd45f414df25e28c0e9f
Roman <romangolovlyov@gmail.com>
2025-07-09 18:35:02 +0500

    added label 123

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -17,8 +17,6 @@
           <label>Email</label>
           <input v-model="email" type="email" placeholder="eg. name@gmail.com" />
 
-          <label>123</label>
-
           <label>Password</label>
           <input v-model="password" type="password" placeholder="eg. xyz1234567" />
 
